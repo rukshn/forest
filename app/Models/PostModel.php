95c6374d82a2999a1534fe3ba56e7086d9e5cbf8
Rukshan <arkruka@gmail.com>
2021-08-17 18:12:10 +0530

    fixed typo

--- app/Models/PostModel.php
+++ app/Models/PostModel.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class PostMdel extends Model
+class PostModel extends Model
 {
     use HasFactory;
     protected $table = 'posts';
