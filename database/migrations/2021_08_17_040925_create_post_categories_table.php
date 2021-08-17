5b38e3eed2ecd9303dc91276973b263194384bc0
Rukshan <arkruka@gmail.com>
2021-08-17 17:14:22 +0530

    Renames file, corrected table name

--- database/migrations/2021_08_17_040925_create_post_categories_table.php
+++ database/migrations/2021_08_17_040925_create_post_categories_table.php
@@ -13,7 +13,7 @@ class CreatePostCatetoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('post_catetories', function (Blueprint $table) {
+        Schema::create('post_categories', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->integer('post_id');
