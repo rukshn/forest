39b63ba872d9ede1892ab06966aa6c6239ff0c5f
Rukshan <arkruka@gmail.com>
2021-08-17 18:11:54 +0530

    post meta table migration

---
+++ database/migrations/2021_08_17_121034_create_post_meta_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostMetaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_meta', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('category_id');
+            $table->integer('post_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_meta');
+    }
+}
