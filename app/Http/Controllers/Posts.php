fdbf1abae34afb063ccd87b18693fa50018d2c78
Rukshan <arkruka@gmail.com>
2021-08-17 18:12:22 +0530

    Create new post

--- app/Http/Controllers/Posts.php
+++ app/Http/Controllers/Posts.php
@@ -1,10 +1,11 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
-use App\Http\Models\PostModel;
+use App\Models\PostModel;
+use App\Models\PostMetaModel;
 
 class Posts extends Controller
 {
@@ -14,35 +15,30 @@ class Posts extends Controller
         $rules = [
             'title' => 'required',
             'post' => 'required',
+            'category' => 'required'
         ];
 
         $validate = Validator::make($request->all(), $rules);
 
         if ($validate->fails()) {
-            return redirect()->back()->with('message', 'Error creating post');
+            return redirect()->back()->with('error', 'Error creating post');
         } else {
-            $user_id = Auth()->id;
+            $user_id = Auth::id();
             $new_post = new PostModel();
             $new_post->title = $request->title;
-            $new_post->content = $request->content;
+            $new_post->post = $request->post;
             $new_post->created_by = $user_id;
             $new_post->save();
+
+            $post_meta = new PostMetaModel;
+            $post_meta->post_id = $new_post->id;
+            $post_category = $request->category;
+
             return redirect()->route('post', ['id' => $new_post->id]);
         }
     }
 
     public function get_post(Request $request) {
-        $rules [
-            'id' => 'required';
-        ];
 
-        $validate = Validator::make($request->all(), $rules);
-
-        if ($validate->fails()) {
-            return redirect()->route();
-        } else {
-            $post = PostModel:select('id', $request->id);
-            return view('post')->with('post_content', $post);
-        }
     }
 }
