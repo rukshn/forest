61f0bf29c67bf6e4f4f0d86e26673017820c6942
Rukshan <arkruka@gmail.com>
2021-08-24 12:16:00 +0530

    Dynamic titles

--- app/View/Components/AppLayout.php
+++ app/View/Components/AppLayout.php
@@ -11,6 +11,16 @@ class AppLayout extends Component
      *
      * @return \Illuminate\View\View
      */
+
+     /**
+      * @var string
+      */
+      public $title;
+
+    public function __construct($title) {
+        $this->title = " - " . $title;
+    }
+
     public function render()
     {
         return view('layouts.app');
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout>
+<x-app-layout title="Dashboard">
     <!-- Page Heading -->
     <header class="bg-white shadow">
         <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
