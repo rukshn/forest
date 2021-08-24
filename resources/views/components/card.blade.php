5040b1d5b6f5f8d34bbbbf3889db262bc96c12d9
Rukshan <arkruka@gmail.com>
2021-08-24 18:20:47 +0530

    Responsive cards

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -3,9 +3,11 @@
     <div class="font-bold col-span-4">
         <a class="hover: text-blue-700" href="/post/{{ $pid }}">{{ $title }}</a>
     </div>
-    <div class="col-span-1"><span class="rounded-lg text-white text-sm font-bold px-2 py-1" style="background-color: #{{$ccolor}}">{{ $category }}<span></div>
-    <div class="col-span-2"><span class="rounded-lg text-white text-sm font-bold px-2 py-1" style="background-color: #{{$scolor}}">{{$status}}</span></div>
-    <div class="col-auto lg:col-span-2 text-gray-500 text-sm">{{ $author }}</div>
-    <div class="col-auto lg:col-span-2 hidden lg:block text-gray-500 text-sm font-light">{{ $date }}</div>
-    <div class="col-span-1 hidden lg:block text-gray-500 text-sm"><i class="bi bi-chat mr-2"></i> {{ $comments}}</div>
+    <div class="col-span-12 lg:col-span-1"><span class="rounded-lg text-white text-sm font-bold px-2 py-1" style="background-color: #{{$ccolor}}">{{ $category }}<span></div>
+    @if($status != '')
+    <div class="col-span-3 lg:col-span-2"><span class="rounded-lg text-white text-sm font-bold px-2 py-1" style="background-color: #{{$scolor}}">{{$status}}</span></div>
+    @endif
+    <div class="col-span-3 lg:col-span-2 text-gray-500 text-sm">{{ $author }}</div>
+    <div class="col-span-2 lg:col-span-2 hidden lg:block text-gray-500 text-sm font-light">{{ $date }}</div>
+    <div class="col-span-2 lg:col-span-1 text-gray-500 text-sm"><i class="bi bi-chat mr-2"></i> {{ $comments}}</div>
 </div>
