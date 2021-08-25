87d19c174149d85d5b7e7896b283083545d32f77
Rukshan <arkruka@gmail.com>
2021-08-25 21:05:10 +0530

    Api kanban route

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Kanban;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/kanban/tasks', [Kanban::class, 'tasks'])->middleware(['api']);
+
+Route::post('/kanban/beginTask', [Kanban::class, 'beginTask'])->middleware(['api']);
