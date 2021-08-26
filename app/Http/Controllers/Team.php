89fe7987186122e1df31bc15ec3043b1ddf2f375
Rukshan <arkruka@gmail.com>
2021-08-27 02:05:44 +0530

    fixed sql duplicate

--- app/Http/Controllers/Team.php
+++ app/Http/Controllers/Team.php
@@ -13,13 +13,16 @@ class Team extends Controller
     public function index(Request $request) {
         $team = User::select('users.name',
                 DB::raw('(select count(*) from asigns where asigns.user_id = users.id) as task_count'),
-                DB::raw('(select count(*) from post_status where post_status.status_id = 2 and post_status.post_id = posts.id) as assigned_tasks'),
-                DB::raw('(select count(*) from post_status where post_status.status_id = 3 and post_status.post_id = posts.id) as completed_tasks')
+                DB::raw('(select count(*) from post_status where post_status.status_id = 2 and post_status.post_id = posts.id)
+                        as assigned_tasks'),
+                DB::raw('(select count(*) from post_status where post_status.status_id = 3 and post_status.post_id = posts.id)
+                        as completed_tasks')
                 )
                 ->leftJoin('asigns', 'users.id', '=', 'asigns.user_id')
-                ->leftJoin('posts', 'posts.id', '=', 'asigns.post_id')
-                ->leftJoin('post_status', 'post_status.post_id', '=', 'posts.id')
-                ->orderBy('task_count', 'asc')
+                ->join('posts', 'posts.id', '=', 'asigns.post_id')
+                ->join('post_status', 'post_status.post_id', '=', 'posts.id')
+                ->orderBy('completed_tasks', 'desc')
+                ->distinct()
                 ->get();
 
         // return json_encode($team);
