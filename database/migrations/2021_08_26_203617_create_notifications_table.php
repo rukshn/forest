7d11758a9aa22f4889779ce6b8e8f71aa297bf15
Rukshan <arkruka@gmail.com>
2021-08-27 02:14:07 +0530

    Create notifications table migration

---
+++ database/migrations/2021_08_26_203617_create_notifications_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->bigInteger('to_user_id');
+            $table->bigInteger('from_user_id');
+            $table->bigInteger('post_id');
+            $table->text('message')->nullable();
+            $table->boolean('has_opened')->default($value=false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notifications');
+    }
+}
