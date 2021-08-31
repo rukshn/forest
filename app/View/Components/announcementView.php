0294f1c27dd38c220bdc2bceab5cf0a51030a4a3
Rukshan <arkruka@gmail.com>
2021-08-31 10:52:15 +0530

    Announcement view initial commit

---
+++ app/View/Components/announcementView.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+use App\Models\AnnouncementModel;
+
+class announcementView extends Component
+{
+    /**
+     * Create a new component instance.
+     *  @var string
+     */
+        public $title;
+    /**
+    *   @var string;
+    */
+        public $announcement;
+    /**
+     *  @var integer;
+     */
+        public $announcement_id;
+    /**
+     *  @var boolean;
+     */
+        public $has_announcement;
+
+    public function __construct()
+    {
+        $get_announcement = AnnouncementModel::where('is_pinned', true)->first();
+        if (isset($get_announcement)) {
+            $this->has_announcement = true;
+            $this->title = $get_announcement->title;
+            $this->announcement = $get_announcement->announcement;
+            $this->announcement_id = $get_announcement->id;
+        } else {
+            $this->has_announcement = false;
+        }
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('components.announcement-view');
+    }
+}
