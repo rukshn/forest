7ce26dd2dc052bcfd4edfa310a8dec31ec249258
Rukshan <arkruka@gmail.com>
2021-08-31 10:52:37 +0530

    Create announcement table migration

---
+++ database/migrations/2021_08_30_144320_create_announcements_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnnouncementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('announcements', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title');
+            $table->text('announcement');
+            $table->bigInteger('created_by');
+            $table->boolean('is_pinned')->default($value=false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('announcements');
+    }
+}
