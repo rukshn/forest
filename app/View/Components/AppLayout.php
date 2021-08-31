5ae985591858d6a8c9fa5a01f612442a312d8c6b
Rukshan <arkruka@gmail.com>
2021-08-31 10:52:28 +0530

    Added announcement to app layout

--- app/View/Components/AppLayout.php
+++ app/View/Components/AppLayout.php
@@ -4,6 +4,7 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 use App\Models\NotificationsModel;
+use App\Models\AnnouncementModel;
 
 class AppLayout extends Component
 {
@@ -22,14 +23,28 @@ class AppLayout extends Component
        */
       public $notification_count;
 
+      /**
+       * @var boolean
+       */
+      public $has_announcement;
     public function __construct($title) {
         $this->title = " - " . $title;
         $this->notification_count = NotificationsModel::where('to_user_id', auth()->user()->id)
             ->where('has_opened', false)->count();
+
+        $announcement = AnnouncementModel::where('is_pinned', true)->first();
+        if (isset($announcement)) {
+            $this->has_announcement = true;
+        } else {
+            $this->has_announcement = false;
+        }
     }
 
     public function render()
     {
-        return view('layouts.app', ['notification_count' => $this->notification_count]);
+        return view('layouts.app', [
+            'notification_count' => $this->notification_count,
+            'announcement' => $this->has_announcement
+        ]);
     }
 }
