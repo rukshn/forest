2e046a3af2c9c86382e701c489e6c36806e7f447
Rukshan <arkruka@gmail.com>
2021-09-03 02:43:08 +0530

    Milestones table create initial commit

---
+++ database/migrations/2021_09_02_200528_create_milestones_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMilestonesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('milestones', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('milestone_id');
+            $table->integer('post_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('milestones');
+    }
+}
