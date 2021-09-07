7e40a91148ed4b99e56cb24aec352547b4d33918
Rukshan <arkruka@gmail.com>
2021-09-07 22:00:43 +0530

    UI Improve announcement card

--- resources/views/components/announcement.blade.php
+++ resources/views/components/announcement.blade.php
@@ -1,8 +1,9 @@
 @if ($hasAnnouncement == true)
-<div class="rounded-md mb-4 bg-indigo-400 text-white px-3 py-3 max-w-7xl mx-auto sm:px-6 lg:px-8">
-    <!-- Knowing is not enough; we must apply. Being willing is not enough; we must do. - Leonardo da Vinci -->
+<div class="rounded-md space-y-2 mb-4 bg-indigo-100 px-3 py-3 max-w-7xl mx-auto sm:px-6 lg:px-8">
     <h1 class="font-bold py-2 text-xl">📣 Announcement - {{ $title ?? '' }}</h1>
-    <a class="bg-indigo-600 rounded-md px-2 py-1 text-gray-100 hover:text-white hover:bg-indigo-800"
-        href="/announcement/{{ $announcementId }}">Read More</a>
+    <div class="flex">
+        <a class="bg-indigo-500 font-bold rounded-md px-2 py-1.5 text-gray-100 hover:text-white hover:bg-indigo-600"
+            href="/announcement/{{ $announcementId }}">Read More</a>
+    </div>
 </div>
 @endif
