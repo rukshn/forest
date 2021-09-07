29a7d2c5e70e6539229defd3a35ed768c1ad73fc
Rukshan <arkruka@gmail.com>
2021-09-07 22:42:38 +0530

    Fixed returning announcement variable name

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -66,7 +66,7 @@ class HomeController extends Controller
         return view('home', [
             'milestones' => $get_milestones,
             'has_announcement' => $has_announcement,
-            'get_announcement' => $get_announcement,
+            'announcement' => $get_announcement,
             'tasks' => $tasks,
             'issues' => $issues
         ]);
