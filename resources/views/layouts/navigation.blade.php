09c1e9c7442f8d6282440c1956a3167eeb612413
Rukshan <arkruka@gmail.com>
2021-09-07 22:00:57 +0530

    Renamed feed to home at navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,7 +13,7 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('🚧 Feed') }}
+                        {{ __('🚧 Home') }}
                     </x-nav-link>
                     <x-nav-link :href="route('issues')" :active="request()->routeIs('issues')">
                         {{ __('👾 Issues') }}
