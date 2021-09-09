6cdfce44e577c220b9b1d613d38a68777b709f1a
Rukshan <arkruka@gmail.com>
2021-09-10 01:24:52 +0530

    Home controller to send reviews to dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -50,6 +50,29 @@ class HomeController extends Controller
         ->orderByDesc('posts.id')
         ->get();
 
+        $reviews = DB::table('qatests')
+            ->join('posts', 'qatests.post_id', '=', 'posts.id')
+            ->join('post_meta', 'posts.id', '=', 'post_meta.post_id')
+            ->leftJoin('testingstates', 'qatests.testing_state', '=', 'testingstates.id')
+            ->leftJoin('post_status', 'posts.id', '=', 'post_status.post_id')
+            ->leftJoin('status_codes', 'post_status.status_id', '=', 'status_codes.id')
+            ->leftJoin('priority_codes', 'posts.priority', '=', 'priority_codes.id')
+            ->join('categories', 'post_meta.category_id', '=', 'categories.id')
+            ->join('users','users.id', '=', 'posts.created_by')
+            ->select('post_meta.category_id',
+            'post_status.status_id as status_code',
+            'status_codes.status_name as status_name', 'status_codes.color as status_color',
+            'categories.name as category_name', 'categories.slug as category_slug', 'categories.color as category_color',
+            'posts.title as post_title', 'posts.created_at as post_date', 'posts.id as post_id',
+            'users.name as user_name', 'users.id as user_id',
+            'priority_codes.priority_code as priority_code', 'priority_codes.color as priority_color',
+            'testingstates.status_name as testing_state_name', 'testingstates.color as testing_state_color',
+            DB::raw('(select count(*) from comments where comments.post_id = posts.id) as comment_count'))
+            ->limit(12)
+            ->orderByDesc('qatests.id')
+            ->get();
+
+
         $get_milestones = DB::table('post_meta')->where('category_id', 3)->where('posts.is_archived', false)
         ->join('posts', 'posts.id', '=', 'post_meta.post_id')
         ->select('posts.id as milestone_id', 'posts.title as milestone')
@@ -68,7 +91,8 @@ class HomeController extends Controller
             'has_announcement' => $has_announcement,
             'announcement' => $get_announcement,
             'tasks' => $tasks,
-            'issues' => $issues
+            'issues' => $issues,
+            'reviews' => $reviews
         ]);
     }
 }
