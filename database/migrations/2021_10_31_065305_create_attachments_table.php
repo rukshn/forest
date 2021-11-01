0ce5bb618ed38aeee5f22b1d910a8d89e111c6ab
Rukshan <arkruka@gmail.com>
2021-11-01 23:59:13 +0530

    Create attachments table to manage file uploads

---
+++ database/migrations/2021_10_31_065305_create_attachments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttachmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attachments', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('filename');
+            $table->bigInteger('post_id');
+            $table->bigInteger('user_id');
+            $table->uuid('uuid');
+            $table->string('extension');
+            $table->boolean('is_archieved')->default($value=false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attachments');
+    }
+}
