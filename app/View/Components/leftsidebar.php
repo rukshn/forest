bd7944e03288c121a71a0974c16b191e9e32a5ee
Rukshan <arkruka@gmail.com>
2021-11-06 23:45:50 +0530

    left sidebar initial commit

---
+++ app/View/Components/leftsidebar.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+use Illuminate\Support\Facades\DB;
+
+class leftsidebar extends Component
+{
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+
+    public $categories;
+    public function __construct()
+    {
+        //
+        $this->categories = DB::table('categories')->where('retired', false)->select('name', 'id', 'color')->get();
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('components.leftsidebar');
+    }
+}
---
+++ resources/views/components/leftsidebar.blade.php
@@ -0,0 +1,19 @@
+<div id="sidebarLeft"
+    class="fixed z-40 inset-0 flex-none h-full bg-black bg-opacity-25 w-full lg:bg-gray-100 lg:static lg:h-auto lg:overflow-y-visible lg:pt-0 lg:w-60 xl:w-72 lg:block hidden">
+    <div class="h-full overflow-y-auto scrolling-touch lg:h-auto lg:block lg:relative lg:sticky lg:bg-transparent overflow-hidden lg:top-18 bg-white mr-24 lg:mr-0"
+        id="sidebar-wrapper">
+        <nav
+            class="px-1 pt-6 overflow-y-auto font-medium text-base sm:px-3 xl:px-5 lg:text-sm pb-10 lg:pt-10 lg:pb-14 sticky?lg:h-(screen-18)">
+            <ul class="px-3">
+                @foreach ($categories as $category)
+                <li class="flex items-center">
+                    <span class="rounded-md h-6 w-6" style="background-color: #{{$category->color}}"></span>
+                    <a href="#" class="px-3 py-2 text-base">
+                        {{ $category->name }}
+                    </a>
+                </li>
+                @endforeach
+            </ul>
+        </nav>
+    </div>
+</div>
